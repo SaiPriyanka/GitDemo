e45739a90bc2f0426ceeaab45e60603ef97e9069
SaiPriyanka <priyanka@192.168.2.143>
2016-02-19 23:51:58 -0800

    adding project java

---
+++ GitHubDemo/src/HelloWorld.java
@@ -0,0 +1,19 @@
+
+
+/***
+ * 
+ * Helloworld
+ * @author priyanka
+ *
+ */
+public class HelloWorld {
+	
+	public void main(String[] args){
+		print();
+	}
+	
+	public void print(){
+		System.out.println("Testing");
+	}
+
+}

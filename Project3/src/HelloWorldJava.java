f9f7b78fb0c07a0d31d58662d6a3f56547e57d37
SaiPriyanka <priyanka@192.168.2.143>
2016-02-21 19:31:38 -0800

    adding project to remote repo

---
+++ Project3/src/HelloWorldJava.java
@@ -0,0 +1,23 @@
+
+
+/***
+ * 
+ * Helloworld
+ * @author priyanka
+ *
+ */
+public class HelloWorldJava {
+	
+	public void main(String[] args){
+		print("Hello World");
+		
+		print("Hello!!!!!This is Project1");
+		
+		
+	}
+	
+	public void print(String text){
+		System.out.println(text+"Testing");
+	}
+
+}
